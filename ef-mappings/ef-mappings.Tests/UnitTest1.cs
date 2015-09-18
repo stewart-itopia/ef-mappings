93c5674d40c8710e9f27850a2917472ef415cdc3
Stewart Mbofana <stewart@itopia.co.za>
2015-09-18 10:55:10 +0200

    Initialize Visual Studio project

---
+++ ef-mappings/ef-mappings.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ef_mappings.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ef-mappings/ef-mappings/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ef_mappings.Global" Language="C#" %>
